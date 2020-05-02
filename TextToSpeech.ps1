0fb90ebfb64c285a4432fe3f843953ef4edbec42
h-marti <43405279+h-marti@users.noreply.github.com>
2020-05-02 19:39:53 +0200

    :sparkles: It just works

---
+++ TextToSpeech.ps1
@@ -0,0 +1,25 @@
+﻿#Script to tell shit
+#Author: Martinou
+
+Add-Type -AssemblyName System.speech
+
+While(1) { 
+    $text = Read-Host 'Say : '
+
+    #Reading
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+    $speak.Speak($text)
+
+    #Decomment to allow saving to wav
+    #$test = Read-Host '----- Save ? ----- (o/n)'
+
+    #if($test -eq "o") {
+
+    #    #Export fichier audio
+    #    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer 
+    #    $speak.SetOutputToWaveFile($text + ".wav")
+    #    $speak.Speak($text)
+    #    $speak.Dispose()
+
+    #}
+}
